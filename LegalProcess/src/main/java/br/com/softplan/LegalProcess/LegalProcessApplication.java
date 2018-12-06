df587f445bce8b1e0abd619fdebee78bb958610f
Gabriel Amoedo <g_amoedo@hotmail.com>
2018-12-05 22:45:27 -0200

    Primeiro commit

---
+++ LegalProcess/src/main/java/br/com/softplan/LegalProcess/LegalProcessApplication.java
@@ -0,0 +1,12 @@
+package br.com.softplan.LegalProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalProcessApplication.class, args);
+	}
+}
---
+++ LegalProcess/src/test/java/br/com/softplan/LegalProcess/LegalProcessApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.softplan.LegalProcess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegalProcessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
