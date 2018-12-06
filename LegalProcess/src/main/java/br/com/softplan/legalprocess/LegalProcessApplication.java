c1f3a5cbb89d40720cc0c7c046b309d7b740f757
Gabriel Amoedo <g_amoedo@hotmail.com>
2018-12-05 23:26:50 -0200

    Subindo o projeto

--- LegalProcess/src/main/java/br/com/softplan/legalprocess/LegalProcessApplication.java
+++ LegalProcess/src/main/java/br/com/softplan/legalprocess/LegalProcessApplication.java
@@ -1,12 +1,12 @@
-package br.com.softplan.LegalProcess;
+package br.com.softplan.legalprocess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LegalProcessApplication {
+public class LegalprocessApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LegalProcessApplication.class, args);
+		SpringApplication.run(LegalprocessApplication.class, args);
 	}
 }
--- LegalProcess/src/test/java/br/com/softplan/LegalProcess/LegalProcessApplicationTests.java
+++ LegalProcess/src/test/java/br/com/softplan/LegalProcess/LegalProcessApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.softplan.LegalProcess;
+package br.com.softplan.legalprocess;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LegalProcessApplicationTests {
+public class LegalprocessApplicationTests {
 
 	@Test
 	public void contextLoads() {
